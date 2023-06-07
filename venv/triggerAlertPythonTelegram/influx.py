e29769f3d1dfa4149bb4bd87f73ed4c92e8d88cb
Sachin Kumar <sachin@Sachins-Air.Dlink>
2023-06-07 19:08:19 +0530

    created new influx alert for health warn

--- venv/triggerAlertPythonTelegram/influx.py
+++ venv/triggerAlertPythonTelegram/influx.py
@@ -10,22 +10,24 @@ chat_id = "5130182527"
 client = InfluxDBClient(host='192.168.0.184', port=8086)
 list_db = client.get_list_database()
 db = client.switch_database('ESB')
-results = client.query('select * from tuxx')
+results = client.query('select last(Method) as Method, apiName from tuxx')
 
 for measurement in results.get_points(measurement='tuxx'):
     method = measurement['Method']
     api = measurement['apiName']
-    counter = measurement['counter']
-    ip = measurement['ip']
-    responseTime = measurement['responseTime']
-    statusCode = measurement['statusCode']
+    # counter = measurement['counter']
+    # ip = measurement['ip']
+    # responseTime = measurement['responseTime']
+    # statusCode = measurement['statusCode']
     print(method, end=' ')
     print(api, end=' ')
-    print(counter, end=' ')
-    print(ip, end=' ')
-    print(responseTime, end=' ')
-    print(statusCode)
+    # print(counter, end=' ')
+    # print(ip, end=' ')
+    # print(responseTime, end=' ')
+    # print(statusCode)
 
-
-
-
+    with open('output.txt', 'w') as f:
+        # for line in total:
+        timestr = time.strftime("%Y-%m-%d %H:%M")
+        f.write(timestr + " " + method + " " + api)
+        print("Done !!")
---
+++ venv/triggerAlertPythonTelegram/sendAlertTelegram.py
@@ -0,0 +1,31 @@
+import time
+import requests
+import os
+
+TOKEN = "5558538416:AAGDhGDtwDOYl-69RgRfNu1JB7U2g6ffVqY"
+chat_id = "5130182527"
+
+if os.path.getsize('output.txt') != 0:
+    print("Yes !!! file has something")
+
+    # while True:
+    data = open('output.txt')
+    count = data.read()
+    print(count)
+    n = count.split(" ")
+    print(n[0])
+    print(n[2])
+    timestr = time.strftime("%Y-%m-%d %H:%M")
+    print("Current Date Time: " + " " + str(timestr))
+
+    if n[2] == "GET":
+        print("Alert Triggered !!")
+        msg_count = "Health Warn Alert :" + "\t" + str(count) + " " + "\U0001F631"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_count}"
+        print(requests.get(url).json())
+        print(msg_count)
+        open('output.txt', 'w').close()
+        print("Now File is Empty")
+
+else:
+    print("file is empty")
